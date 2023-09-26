f652694e9c1ffdf09e74947dc65a9f741a77a2bd
agenteperreo <igarcar@iesnervion.es>
2023-09-26 14:37:00 +0200

    26/09/2023

---
+++ interfaces/Tema3/Ejercicio1_Tema3/HolaMundo_VB_WindowsForm/Form1.vb
@@ -0,0 +1,9 @@
+﻿Public Class Form1
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+    End Sub
+
+    Private Sub btnSaludar_Click(sender As Object, e As EventArgs) Handles btnSaludar.Click
+        MessageBox.Show("psjhbns", "saludo")
+    End Sub
+End Class
---
+++ interfaces/Tema3/Ejercicio1_Tema3/LibreriaComun_C#/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace LibreriaComun_C_
+{
+    public class Class1
+    {
+
+    }
+}
