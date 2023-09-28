e7fd331a4eb98fdd58dc617f3a5219f268a6b662
agenteperreo <igarcar@iesnervion.es>
2023-09-28 13:28:28 +0200

    28/09/2023

--- interfaces/Tema3/Ejercicio1_Tema3/HolaMundo_C#_WPF/obj/Debug/net7.0-windows/MainWindow.g.i.cs
+++ interfaces/Tema3/Ejercicio1_Tema3/HolaMundo_C#_WPF/obj/Debug/net7.0-windows/MainWindow.g.i.cs
@@ -53,7 +53,7 @@ namespace HolaMundo_C__WPF {
                 return;
             }
             _contentLoaded = true;
-            System.Uri resourceLocater = new System.Uri("/HolaMundo_Csharp_WPF;component/mainwindow.xaml", System.UriKind.Relative);
+            System.Uri resourceLocater = new System.Uri("/HolaMundo_Csharp_WPF;V1.0.0.0;component/mainwindow.xaml", System.UriKind.Relative);
             
             #line 1 "..\..\..\MainWindow.xaml"
             System.Windows.Application.LoadComponent(this, resourceLocater);
