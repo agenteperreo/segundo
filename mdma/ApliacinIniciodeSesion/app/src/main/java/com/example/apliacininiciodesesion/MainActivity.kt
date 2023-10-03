a74d0167f3112d8a9eef0a788f9bec83704b9720
agenteperreo <igarcar@iesnervion.es>
2023-10-03 21:27:08 +0200

    03/10/2023

--- mdma/ApliacinIniciodeSesion/app/src/main/java/com/example/apliacininiciodesesion/MainActivity.kt
+++ mdma/ApliacinIniciodeSesion/app/src/main/java/com/example/apliacininiciodesesion/MainActivity.kt
@@ -1,8 +1,14 @@
 package com.example.apliacininiciodesesion
 
+import android.app.NotificationChannel
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Intent
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import androidx.core.app.NotificationCompat
 import com.example.apliacininiciodesesion.databinding.ActivityMainBinding
 import com.example.apliacininiciodesesion.databinding.BienvenidaBinding
 import com.example.apliacininiciodesesion.databinding.PausaBinding
@@ -11,9 +17,12 @@ class MainActivity : AppCompatActivity() {
 
     var nombre = ""
 
+    var contasena = ""
+
     var pausa = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
 
         val activityMain = ActivityMainBinding.inflate(layoutInflater)
@@ -24,13 +33,20 @@ class MainActivity : AppCompatActivity() {
 
         var boton = activityMain.btnLogin
 
-        boton.setOnClickListener {
-            nombre = activityMain.txtUsuarioInput.text.toString()
-            setContentView(bienvenida.root)
-            bienvenida.txtBienvenida.text = "Nos alegramos de que vuelvas, $nombre"
-        }
-    }
+        nombre = activityMain.txtUsuarioInput.text.toString()
 
+        boton.setOnClickListener {
+            contasena = activityMain.txtContraseAInput.text.toString()
+            if(contasena.equals("abc123")) {
+                setContentView(bienvenida.root)
+                bienvenida.txtBienvenida.text = "Nos alegramos de que vuelvas, $nombre"
+            } else {
+                Toast.makeText(applicationContext, "Contraseña incorrecta", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+    }
     override fun onPause() {
         super.onPause()
 
@@ -38,18 +54,53 @@ class MainActivity : AppCompatActivity() {
 
         val pausa = PausaBinding.inflate(layoutInflater)
 
-        setContentView(pausa.root)
+        if(contasena.equals("abc123")){
+            setContentView(pausa.root)
+        }
+
     }
     override fun onResume() {
         super.onResume()
 
         Toast.makeText(applicationContext, "Bienvenido de nuevo $nombre", Toast.LENGTH_SHORT).show()
 
+        val bienve = BienvenidaBinding.inflate(layoutInflater)
+
+        if (contasena.equals("abc123")) {
+            setContentView(bienve.root)
+        }
     }
 
     override fun onStop() {
         super.onStop()
 
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            val channelId = "mi_canal_id"
+            val channelName = "Mi Canal de Notificación"
+            val importance = NotificationManager.IMPORTANCE_DEFAULT
+            val channel = NotificationChannel(channelId, channelName, importance)
+
+            val notificationManager = getSystemService(NotificationManager::class.java)
+            notificationManager.createNotificationChannel(channel)
+        }
+
+        // Crea un intent para abrir una actividad cuando se toque la notificación
+        val intent = Intent(this, MainActivity::class.java)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+
+// Crea la notificación
+        val notificationBuilder = NotificationCompat.Builder(this, "mi_canal_id")
+            .setSmallIcon(R.drawable.ic_notificacion)
+            .setContentTitle("Cierre de sesión")
+            .setContentText("Tienes que iniciar sesión cuando vuelvas")
+            .setContentIntent(pendingIntent)
+            .setAutoCancel(true) // Cierra la notificación al tocarla
+            .setPriority(NotificationCompat.PRIORITY_DEFAULT)
+
+// Muestra la notificación
+        val notificationManager = getSystemService(NotificationManager::class.java)
+        notificationManager.notify(1, notificationBuilder.build())
 
     }
+
 }
