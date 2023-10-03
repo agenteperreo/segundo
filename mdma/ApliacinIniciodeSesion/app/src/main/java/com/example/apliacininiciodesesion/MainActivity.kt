f78dea25a0c9fb9b9a9eb9e2ab9b09951196fb37
agenteperreo <igarcar@iesnervion.es>
2023-10-03 10:20:43 +0200

    03/10/2023

--- mdma/ApliacinIniciodeSesion/app/src/main/java/com/example/apliacininiciodesesion/MainActivity.kt
+++ mdma/ApliacinIniciodeSesion/app/src/main/java/com/example/apliacininiciodesesion/MainActivity.kt
@@ -2,10 +2,54 @@ package com.example.apliacininiciodesesion
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.example.apliacininiciodesesion.databinding.ActivityMainBinding
+import com.example.apliacininiciodesesion.databinding.BienvenidaBinding
+import com.example.apliacininiciodesesion.databinding.PausaBinding
 
 class MainActivity : AppCompatActivity() {
+
+    var nombre = ""
+
+    var pausa = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        val activityMain = ActivityMainBinding.inflate(layoutInflater)
+
+        val bienvenida = BienvenidaBinding.inflate(layoutInflater)
+
+        setContentView(activityMain.root)
+
+        var boton = activityMain.btnLogin
+
+        boton.setOnClickListener {
+            nombre = activityMain.txtUsuarioInput.text.toString()
+            setContentView(bienvenida.root)
+            bienvenida.txtBienvenida.text = "Nos alegramos de que vuelvas, $nombre"
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+
+        Toast.makeText(applicationContext, "He parado", Toast.LENGTH_SHORT).show()
+
+        val pausa = PausaBinding.inflate(layoutInflater)
+
+        setContentView(pausa.root)
+    }
+    override fun onResume() {
+        super.onResume()
+
+        Toast.makeText(applicationContext, "Bienvenido de nuevo $nombre", Toast.LENGTH_SHORT).show()
+
+    }
+
+    override fun onStop() {
+        super.onStop()
+
+
     }
 }
