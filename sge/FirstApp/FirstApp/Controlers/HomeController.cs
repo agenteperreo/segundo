d094dc7ba4d54c93070b7d6a99584875a075bf62
agenteperreo <igarcar@iesnervion.es>
2023-10-18 14:41:54 +0200

    18/10/2023

---
+++ sge/FirstApp/FirstApp/Controlers/HomeController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApp.Controlers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public String Apellido()
+        {
+            return "García Carmona";
+        }
+
+        public IActionResult saludo()
+        {
+            return View();
+        }
+    }
+}
---
+++ sge/FirstApp/FirstApp/Controlers/ProductosController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace FirstApp.Controlers
+{
+    public class ProductosController : Controller
+    {
+        public IActionResult ListadoProductos()
+        {
+            return View();
+        }
+
+        
+    }
+}
