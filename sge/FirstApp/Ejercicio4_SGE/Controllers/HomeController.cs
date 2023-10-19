389628fb279b472d14bf1ca6bb54913b01eef6ab
agenteperreo <igarcar@iesnervion.es>
2023-10-19 10:47:39 +0200

    19/10/2023

---
+++ sge/FirstApp/Ejercicio4_SGE/Controllers/HomeController.cs
@@ -0,0 +1,16 @@
+﻿using Ejercicio4_SGE.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace Ejercicio4_SGE.Controllers
+{
+    public class HomeController : Controller
+    {
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
