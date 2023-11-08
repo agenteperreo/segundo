2f91a9666be1f31cda3c132c24a8cf7f54375fea
agenteperreo <igarcar@iesnervion.es>
2023-11-08 14:30:41 +0100

    08/11/2023

---
+++ sge/Tema7SGE/Tema7SGE/Models/DAL/listaDepartamento.cs
@@ -0,0 +1,21 @@
+﻿using Tema7SGE.Models.Entidades;
+
+namespace Tema7SGE.Models.DAL
+{
+    public class listaDepartamento
+    {
+        public static List<clsDepartamento> listadoCompletoDepartamento()
+        {
+            List<clsDepartamento> listaDepartamentos = new List<clsDepartamento>()
+            {
+                new clsDepartamento(1, "Finanzas"),
+                new clsDepartamento(2, "Marketing"),
+                new clsDepartamento(3, "Cadena de suministros"),
+                new clsDepartamento(4, "Produccion"),
+                new clsDepartamento(5, "Marketing Online")
+            };
+
+            return listaDepartamentos;
+        }
+    }
+}
---
+++ sge/Tema7SGE/Tema7SGE/Models/ViewModels/EditarPersonaVM.cs
@@ -0,0 +1,27 @@
+﻿using Tema7SGE.Models.DAL;
+using Tema7SGE.Models.Entidades;
+
+namespace Tema7SGE.Models.ViewModels
+{
+    public class EditarPersonaVM:clsPersona
+    {
+
+        #region Atributos
+        private List<clsDepartamento> departamentos;
+        #endregion
+
+        #region Constructores
+        public EditarPersonaVM() 
+        { 
+            this.departamentos = listaDepartamento.listadoCompletoDepartamento();
+        }
+        #endregion
+
+        #region Propiedades
+        public List<clsDepartamento> Departamentos
+        {
+            get { return departamentos; }
+        }
+        #endregion
+    }
+}
