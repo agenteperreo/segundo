5c78ba51ab6fa9b9c6261fde9f9d5d5e6d4baa0d
agenteperreo <igarcar@iesnervion.es>
2023-11-14 14:13:49 +0100

    14/11/2023

---
+++ interfaces/09_Ejercicios/09_Ejercicio1/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace _09_Ejercicio1
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
