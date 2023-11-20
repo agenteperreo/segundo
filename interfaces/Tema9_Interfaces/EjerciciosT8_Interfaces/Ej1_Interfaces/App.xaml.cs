c0f12f6577dacb348954100077bd1c46ef00d647
agenteperreo <igarcar@iesnervion.es>
2023-11-20 17:40:49 +0100

    20/11/2023

---
+++ interfaces/Tema9_Interfaces/EjerciciosT8_Interfaces/Ej1_Interfaces/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace Ej1_Interfaces
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ interfaces/Tema9_Interfaces/EjerciciosT8_Interfaces/Ej1_Interfaces/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace Ej1_Interfaces
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
