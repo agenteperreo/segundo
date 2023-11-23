d8b334e971a2cd92060e754d90e64db4657b36de
agenteperreo <igarcar@iesnervion.es>
2023-11-23 12:37:58 +0100

    23/11/2023

---
+++ sge/AccesoDatabase/AccesoDatabase/Controllers/HomeController.cs
@@ -0,0 +1,97 @@
+﻿using AccesoDatabase.Models;
+using Entidades;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.SqlClient;
+using System.Diagnostics;
+
+namespace AccesoDatabase.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public ActionResult Index()
+        {
+
+            SqlConnection miConexion = new SqlConnection();
+            
+
+            ViewBag.ConnectionState = "No se ha intentado conectar";
+
+            try
+            {
+                miConexion.ConnectionString = "server=baseisaacazure.database.windows.net;database=IsaacAzure;uid=prueba;pwd=fernandoG321;trustServerCertificate=true";
+
+                miConexion.Open();
+
+                ViewBag.ConnectionState = $"Conectado: {miConexion.State}";
+            }
+            catch (Exception ex)
+            {
+                ViewBag.ConnectionState = $"Error al conectar: {ex.Message}";
+            }
+
+            return View();
+        }
+
+        public ActionResult listadoPersonas()
+        {
+            try
+            {
+                List<clsPersona> listadoCompPersonas = DAL.clsListadoPersonas.listadoCompletoPersonas();
+                return View(listadoCompPersonas);
+            } catch (Exception ex)
+            {
+                ViewBag.Error = "Ha ocurrido un error. Intentelo de nuevo mas tarde";
+                return View("Error");
+            }
+        }
+
+        public ActionResult Delete(int id, List<clsPersona> listadoPersonas)
+        {
+            try
+            {
+                clsPersona oPersona = DAL.personaPorId.obtenerPersonaId(id, listadoPersonas);
+                return View(oPersona);
+            } catch (Exception ex)
+            {
+                ViewBag.Error = "Ha ocurrido un error";
+                return View("Error");
+            }
+        }
+
+        [HttpPost]
+        public ActionResult borrar(clsPersona persona) 
+        {
+            try
+            {
+                int cantRows = DAL.clsDeletePersona.deletePersonaDAL(persona.Id);
+
+                ViewBag.cantRows = cantRows;
+
+                return View();
+            }
+            catch (Exception ex)
+            {
+                ViewBag.Error = "Ha ocurrido un error";
+                return View("Error");
+            }
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ sge/AccesoDatabase/DAL/clsDeletePersona.cs
@@ -0,0 +1,51 @@
+﻿using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL
+{
+    public class clsDeletePersona
+    {
+        public static int deletePersonaDAL(int id)
+
+        {
+
+            int numeroFilasAfectadas = 0;
+
+            SqlConnection miConexion = new SqlConnection();
+
+            SqlCommand miComando = new SqlCommand();
+
+            miConexion.ConnectionString = "server=baseisaacazure.database.windows.net;database=IsaacAzure;uid=prueba;pwd=fernandoG321;trustServerCertificate=true";
+
+            miComando.Parameters.Add("@id", System.Data.SqlDbType.Int).Value = id;
+
+            try
+            {
+
+                miConexion.Open();
+
+                miComando.CommandText = "DELETE FROM personas WHERE ID=@id";
+
+                miComando.Connection = miConexion;
+
+                numeroFilasAfectadas = miComando.ExecuteNonQuery();
+
+            }
+
+            catch (Exception ex)
+
+            {
+
+                throw ex;
+
+            }
+
+            return numeroFilasAfectadas;
+
+        }
+    }
+}
