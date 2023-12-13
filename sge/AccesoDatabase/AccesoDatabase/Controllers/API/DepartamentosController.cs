5b1cc0e3e1fce5dbcd9c3de04a53eeebe06b0b22
agenteperreo <igarcar@iesnervion.es>
2023-12-13 14:37:01 +0100

    13/12/2023

---
+++ sge/AccesoDatabase/AccesoDatabase/Controllers/API/DepartamentosController.cs
@@ -0,0 +1,45 @@
+﻿using BL.Listados;
+using Entidades;
+using Microsoft.AspNetCore.Mvc;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AccesoDatabase.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DepartamentosController : ControllerBase
+    {
+        // GET: api/<DepartamentosController>
+        [HttpGet]
+        public IEnumerable<clsDepartamento> Get()
+        {
+            return clsListaDepartamentosBL.listadoCompletoDepartamentosBL();
+        }
+
+        // GET api/<DepartamentosController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<DepartamentosController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<DepartamentosController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<DepartamentosController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ sge/AccesoDatabase/AccesoDatabase/Controllers/API/PersonasController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using BL;
+using BL.Listados;
+using Entidades;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace AccesoDatabase.Controllers.API
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PersonasController : ControllerBase
+    {
+        // GET: api/<PersonasController>
+        [HttpGet]
+        public IEnumerable<clsPersona> Get()
+        {
+            return clsListaPersonasBL.listadoCompletoPersonasBL();
+        }
+
+        // GET api/<PersonasController>/5
+        [HttpGet("{id}")]
+        public string Get(int id)
+        {
+            return "value";
+        }
+
+        // POST api/<PersonasController>
+        [HttpPost]
+        public void Post([FromBody] string value)
+        {
+        }
+
+        // PUT api/<PersonasController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<PersonasController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
